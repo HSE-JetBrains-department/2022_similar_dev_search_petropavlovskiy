14f799a242fe2d729805e3e76639ae3ace84b13e
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2022-05-27 04:01:43 +0300

    [github-actions] clear ci setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setuptools.setup(
     name="similar_dev_search_petropavlovskiy",
     version="0.1",
     description="Similar developers search poject",
-    setup_requires=["fire", "dulwich", "tqdm", "enry=0.1.0"],
+    setup_requires=["fire", "dulwich", "tqdm"],
     packages=setuptools.find_packages(),
-    install_requires=["fire", "dulwich", "tqdm", "enry=0.1.0"]
+    install_requires=["fire", "dulwich", "tqdm"]
 )
