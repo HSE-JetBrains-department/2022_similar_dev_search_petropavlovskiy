c0411962e78d8ef6e84ac2a59663dc523a66a64a
alf3ratz <49417479+alf3ratz@users.noreply.github.com>
2022-05-27 03:47:42 +0300

    [github-actions] add ci

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="similar_dev_search_petropavlovskiy",
+    version="0.1",
+    description="Similar developers search poject",
+    setup_requires=["fire", "dulwich", "tqdm"],
+    packages=setuptools.find_packages(),
+    install_requires=["fire", "dulwich", "tqdm"]
+)
