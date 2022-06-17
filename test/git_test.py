17316b56903f9532febd2bc8662bec1cc09ec81d
alf3ratz <apetropavlovskij@yandex.ru>
2022-06-17 06:55:29 +0300

    [cad-ci] add tests

--- test/git_test.py
+++ test/git_test.py
@@ -1,10 +1,31 @@
+import logging
+import os
 import unittest
 
+from pathlib2 import Path
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)  # add assertion here
+from src.git.helpers import get_repository_info
+
+logger = logging.getLogger(__name__)
+
+
+class GitTest(unittest.TestCase):
+    def test_repository_info_processing(self):
+        test_repo_url = "https://github.com/MicrosoftDocs/pipelines-python-django"
+        repo_name = "test_repo"
+        repo_info = get_repository_info(test_repo_url, repo_name)
+        print(repo_info)
+        self.assertTrue(repo_info["commits"]["7267bb437c1a1a66ab36b9dc31ed5aee9f84a278"] != None)
+        delete_cloned_repo(repo_name)
 
 
 if __name__ == '__main__':
     unittest.main()
+
+
+def delete_cloned_repo(repo_name: str):
+    try:
+        repo_path = str(Path(f"{Path().cwd().parent}/repos/{repo_name}"))
+        os.rmdir(repo_path)
+    except:
+        logger.info("Can't delete cloned test repository")
