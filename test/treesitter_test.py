ca4619faf14531e24a3f8f4d1c9ea673d40313af
alf3ratz <apetropavlovskij@yandex.ru>
2022-06-17 13:02:01 +0300

    [cad-ci] update

--- test/treesitter_test.py
+++ test/treesitter_test.py
@@ -8,7 +8,10 @@ from src.code_processing.treesitter import process_identifiers, setup_tree_sitte
 class TreeSitterTest(unittest.TestCase):
     def test_identifiers(self):
         setup_tree_sitter_parser()
-        path_to_file = str(Path(f"{Path.cwd()}/test_files/qwerty.java"))
+        cwd = Path.cwd()
+        if cwd.name != "tests":
+            cwd = str(Path(f"{cwd}/tests"))
+        path_to_file = str(Path(f"{cwd}/test_files/qwerty.java"))
         code_info = process_identifiers(path_to_file, "java")
         self.assertEqual(code_info["classes"]["Employee"], 1, "Found one class")
 
