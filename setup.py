08f3fc1bf8db22fbf003ddefb1cd09d88157db37
alf3ratz <apetropavlovskij@yandex.ru>
2022-06-19 01:31:57 +0300

    [cad-docker] fix flake warnings

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 import setuptools
-from pip._internal.req import parse_requirements
 
-install_reqs = parse_requirements('requirements.txt', session='hack')
+install_reqs = ["fire",
+                "dulwich",
+                "tqdm",
+                "PyGithub",
+                "pathlib2",
+                "setuptools",
+                "pip",
+                "tree-sitter",
+                "enry"]
 
 setuptools.setup(
     name="similar_dev_search_petropavlovskiy",
